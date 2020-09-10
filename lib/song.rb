abdfb409b38257c14e7269249a43ac4b03d6cc67
Haidy  Farahat <haidyfarahat@gmail.com>
2020-09-10 03:12:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -42,7 +42,7 @@ class Song
   end
 
   def new_from_filename(filename)
-    split_filename = filename.chomp(".mp3"). split("-"")
+    split_filename = filename.chomp(".mp3"). split("-")
   end
 
   def self.destroy_all
